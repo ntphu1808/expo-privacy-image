12c85b87b641570d6914aa120c2faaac1f044ef3
ntphu <ntphu081995@gmail.com>
2026-04-13 23:06:17 +0700

    fix privacy image wont show on android 16 17

--- android/src/main/java/expo/modules/privacyimage/PrivacyImageModule.kt
+++ android/src/main/java/expo/modules/privacyimage/PrivacyImageModule.kt
@@ -1,6 +1,7 @@
 package expo.modules.privacyimage
 
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import android.widget.ImageView
 import expo.modules.kotlin.exception.Exceptions
 import expo.modules.kotlin.modules.Module
@@ -8,47 +9,56 @@ import expo.modules.kotlin.modules.ModuleDefinition
 
 class PrivacyImageModule : Module() {
 
-  private var isPrivacyEnabled: Boolean = false
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
   private val safeCurrentActivity
     get() = appContext.currentActivity
   private val currentActivity
     get() = safeCurrentActivity ?: throw Exceptions.MissingActivity()
 
   private var overlayView: ImageView? = null
-
-  private fun showOverlay() {
+  private var focusChangeListener: ViewTreeObserver.OnWindowFocusChangeListener? = null
+  
+  private fun setupOverlay() {
     currentActivity.runOnUiThread {
       if (overlayView != null) return@runOnUiThread
 
-      val overlay =
-              ImageView(currentActivity).apply {
-                val drawableId =
-                        currentActivity.resources.getIdentifier(
-                                "privacy_image",
-                                "drawable",
-                                currentActivity.packageName
-                        )
-                setImageResource(drawableId)
-                scaleType = ImageView.ScaleType.CENTER_CROP
-                layoutParams =
-                        ViewGroup.LayoutParams(
-                                ViewGroup.LayoutParams.MATCH_PARENT,
-                                ViewGroup.LayoutParams.MATCH_PARENT
-                        )
-              }
+      val drawableId = currentActivity.resources.getIdentifier(
+        "privacy_image",
+        "drawable",
+        currentActivity.packageName
+      )
+
+      val overlay = ImageView(currentActivity).apply {
+        setImageResource(drawableId)
+        scaleType = ImageView.ScaleType.CENTER_CROP
+        layoutParams = ViewGroup.LayoutParams(
+          ViewGroup.LayoutParams.MATCH_PARENT,
+          ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        alpha = 0f
+        elevation = Float.MAX_VALUE
+      }
 
       val rootView = currentActivity.window.decorView as ViewGroup
       rootView.addView(overlay)
       overlayView = overlay
+
+      val listener = ViewTreeObserver.OnWindowFocusChangeListener { hasFocus ->
+        overlay.alpha = if (hasFocus) 0f else 1f
+      }
+      rootView.viewTreeObserver.addOnWindowFocusChangeListener(listener)
+      focusChangeListener = listener
     }
   }
 
-  private fun hideOverlay() {
+  private fun teardownOverlay() {
     currentActivity.runOnUiThread {
-      val rootView = currentActivity.window.decorView as ViewGroup
+      val rootView = currentActivity.window?.decorView as? ViewGroup ?: return@runOnUiThread
+      focusChangeListener?.let {
+        if (rootView.viewTreeObserver.isAlive) {
+          rootView.viewTreeObserver.removeOnWindowFocusChangeListener(it)
+        }
+        focusChangeListener = null
+      }
       overlayView?.let {
         rootView.removeView(it)
         overlayView = null
@@ -57,27 +67,14 @@ class PrivacyImageModule : Module() {
   }
 
   override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a
-    // string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for
-    // clarity.
-    // The module will be accessible from `requireNativeModule('PrivacyImage')` in JavaScript.
     Name("PrivacyImage")
 
-    Function("usePrivacyImage") { isPrivacyEnabled = true }
-
-    OnActivityEntersBackground {
-      // Show privacy overlay
-      if (!isPrivacyEnabled) return@OnActivityEntersBackground
-
-      showOverlay()
+    Function("usePrivacyImage") {
+      setupOverlay()
     }
 
-    OnActivityEntersForeground {
-      // Remove privacy overlay
-      if (!isPrivacyEnabled) return@OnActivityEntersForeground
-
-      hideOverlay()
+    OnDestroy {
+      teardownOverlay()
     }
   }
 }
